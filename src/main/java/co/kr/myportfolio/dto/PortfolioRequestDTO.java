718f092ea8cab993a82e2780828f9f5e7734f5ac
이건우 <qkskck3@gmail.com>
2025-04-17 16:58:47 +0900

    id 추가

--- src/main/java/co/kr/myportfolio/dto/PortfolioRequestDTO.java
+++ src/main/java/co/kr/myportfolio/dto/PortfolioRequestDTO.java
@@ -14,6 +14,7 @@ public class PortfolioRequestDTO {
     private int userPid;
     @Setter
     private String userNickname;
+    private final int portfolioId;
     private final String title;
     private final String content;
     private final List<String> tags;
@@ -23,11 +24,13 @@ public class PortfolioRequestDTO {
     DTO를 수정 불가능 한 구조를 보장하면서도 Jackson과 호환*/
     @JsonCreator
     public PortfolioRequestDTO(
+            @JsonProperty("portfolioId") int portfolioId,
             @JsonProperty("title") String title,
             @JsonProperty("content") String content,
             @JsonProperty("tags") List<String> tags,
             @JsonProperty("thumbnail") String thumbnail
     ) {
+        this.portfolioId = portfolioId;
         this.title = title;
         this.content = content;
         this.tags = tags;
