0e311db2dc00ef8474c3835fc1ef2c862d2d1ae7
이건우 <qkskck3@gmail.com>
2025-04-27 17:50:30 +0900

    개인 글 페이지 및 좋아요 표시한 글 보기 페이지 이동 엔드포인트 추가

---
+++ src/main/java/co/kr/myportfolio/controller/PersonalController.java
@@ -0,0 +1,43 @@
+package co.kr.myportfolio.controller;
+
+import co.kr.myportfolio.service.PortfolioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/personal")
+public class PersonalController {
+    @Autowired
+    private PortfolioService portfolioService;
+
+    // 특정개인 페이지로
+    @GetMapping("/{userId}")
+    public String goPersonalPage(@PathVariable int userId, Model model) {
+        model.addAttribute("userPid", userId);
+        return "personalPage";
+    }
+
+    // 하트 누른 글 보기
+    @GetMapping("/liked")
+    public String goLikePostPage(HttpSession session,
+                                 RedirectAttributes redirectAttributes) {
+        // 세션에서 유저 정보 가져오기
+        Integer userPid = (Integer) session.getAttribute("user_pid");
+
+        if (userPid == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", "로그인이 필요합니다.");
+            return "redirect:/login";
+        }
+
+        return "likedPostPage";
+    }
+
+}
